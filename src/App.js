bb9caab79fc16f72cb7bd006c988ba669a2f8836
rajimanu1626 <rajimanu1626@gmail.com>
2020-10-14 12:12:39 +0530

    implemented firebase utils, including ability to store authenticated user into our firstore data base

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sigh-in-and-signup/sign-in-and-signup.component'
-import {auth} from './firebase/firebase.utils';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -22,9 +22,24 @@ constructor(props){
 
 
 componentDidMount() {
-  this.unsubscribeFromAuth=auth.onAuthStateChanged(user=>{
-    this.setState({currentUser:user});
-    console.log(user);
+  this.unsubscribeFromAuth=auth.onAuthStateChanged(async userAuth=>{
+    if(userAuth){
+      const userRef=await createUserProfileDocument(userAuth);
+
+      userRef.onSnapshot(snapShot=>{
+        this.setState({
+          currentUser:{
+            id:snapShot.id,
+            ...snapShot.data(),
+          }
+        });
+
+        console.log(this.state);
+      });
+      
+    }
+    else
+    this.setState({currentUser:userAuth});
   })
 }
 
--- src/pages/sigh-in-and-signup/sign-in-and-signup.component.jsx
+++ src/pages/sigh-in-and-signup/sign-in-and-signup.component.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './sign-in-and-signup.styles.scss';
 
 import SignIn from '../../components/sign-in/sign-in.component';
-import Signin from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component'
+
 
 
 const SignInAndSigUpPage =()=>(
 <div className='sign-in-and-sign-up'>
-<Signin/>
+<SignIn/>
+<SignUp/>
 </div>
 
 );
